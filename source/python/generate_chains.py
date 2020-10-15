d0fb3239f9e5aaf3564576130c20d04b6ecdc517
LX-6 <chemin.e1502058@etud.univ-ubs.fr>
2020-10-15 15:54:03 +0200

    Cleaned rubbish stuff, add length parameter, writing table in the file after all the table has been generated

--- source/python/generate_chains.py
+++ source/python/generate_chains.py
@@ -5,54 +5,71 @@ import string
 import random
 
 #Set de caractères possible pour le mot de passe à trouver
-chars="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+chars = string.ascii_letters + string.digits
 chars_len = len(chars)
 
-#Transforme le hash en une chaîne de caractères
-def reduce(hashed, length):
-    i = int(hashed,16)
-    passwd = ""
-    while len(passwd) < length:
-        passwd += chars[i % chars_len]
-        i = i // chars_len
-    return passwd
+#Génère un mot de passe d'une longueur donnée
+def generate_head(length):
+    return ''.join(random.choice(chars) for i in range(length))
 
-#Créé une chaine en partant d'une string "head" et retourne le dernier élément de la chaine "tail"
+#Créé une chaine en partant d'une string "head" et retourne le dernier élément de la chaine ("tail")
 def create_chain(password, nb_boucle, longueur):
-    #nb_boucle == nombre de colonne de la chaine
     for i in range(nb_boucle):
         #On hash le mot de passe
         hashed = hashlib.sha256(password.encode('ascii')).hexdigest()
-        #On applique la fonction de réduction sur le hash du mot de passe 
-        password = reduce(hashed, longueur)
+        #On applique la fonction de réduction sur le hash du mot de passe
+        password = reduce(hashed, longueur, int(i))
 
     #On retourne le hash du dernier password généré
     return hashlib.sha256(password.encode('ascii')).hexdigest()
 
-#Ecrit une chaine avec la tete et la queue générées précédemment dans le fichier de sortie
-def write_chain_to_file(head, tail, output_filename):
-    with open(output_filename, "a") as output_file:
-        prepared_line = head + "-" + tail + "\n"
-        output_file.write(prepared_line)
+#Transforme le hash en une chaîne de caractères
+def reduce(hashed, length, i_col):
+    #On génère une clé à partir du hash et de l'indice de la colonne
+    key = (int(hashed[:9], 16) ^ i_col) % (chars_len ** length)
+    passwd = ""
+    #On itère pour la taille du mot de passe souhaité
+    for i in range(length):
+        #On ajoute le caractère qui correspond à l'indice dans notre set de caractères
+        passwd += chars[key % chars_len]
+        key //= chars_len
+    return passwd
 
-#Génère un mot de passe d'une longueur donnée
-def generate_head(length):
-    characters = string.ascii_letters + string.digits
-    return ''.join(random.choice(characters) for i in range(length))
+#Ecrit la table dans le fichier de sortie
+def write_table_to_file(table, output_filename):
+    with open(output_filename, "a") as output_file:
+        for chain in table:
+            prepared_line = chain[0] + "-" + chain[1] + "\n"
+            output_file.write(prepared_line)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--column", help="Column number", type=int, default=50000)
     parser.add_argument("-ch", "--chain", help="Chains number", type=int, default=500)
+    parser.add_argument("-mn", "--min", help="Minimum length of the password to crack", type=int, default=8)
+    parser.add_argument("-mx", "--max", help="Maximum length of the password to crack", type=int, default=12)
     parser.add_argument("-o", "--output", help="Output file", required=True)
     arguments = parser.parse_args()
 
+    #On écrit le nombre de colonne dans la première ligne du fichier pour le réutiliser pour l'attaque
+    with open(arguments.output, "a") as output_file:
+        prepared_line = str(arguments.column) + "\n"
+        output_file.write(prepared_line)
+
+    #Initialisation de la liste permettant de stocker tous les heads et les tails
+    table_array = list()
+
     #On génère les chaines pour chaque longueur de mot de passe possible, ici minimum 8 et max 12 caractères
-    for length in range(8,13):
+    for length in range(arguments.min,arguments.max+1):
         for i in range(arguments.chain):
             #Obtient la tete de notre chaine
             head = generate_head(length)
             #Obtient la queue de notre chaine
             tail = create_chain(head, arguments.column, length)
-            #Ecrit la chaine dans le fichier de sortie
-            write_chain_to_file(head, tail, arguments.output)
+            #Initialisation de la liste permettant de stocker le head et le tail
+            chain_array = list()
+            chain_array.append(head)
+            chain_array.append(tail)
+            table_array.append(chain_array)
+    #Ecrit la table dans le fichier de sortie
+    write_table_to_file(table_array, arguments.output)
